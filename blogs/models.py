81de4405f4085626471a4e2af27916181c2dd7c4
Preet <preetprajapati8394@gmail.com>
2025-09-18 15:53:50 +0530

    posts by category

--- blogs/models.py
+++ blogs/models.py
@@ -32,5 +32,5 @@ class Blog(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now = True)
 
-def __str__(self):
-  return self.title
+  def __str__(self):
+    return self.title
